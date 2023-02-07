3f3882256c3c733ff1198a95f08cdda171b36c14
Le Tan Phat <phatb1906740@student.ctu.edu.vn>
2023-02-07 20:50:31 +0700

    Tạo route size

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const adminRouter = require('./admin');
 const colourRouter = require('./colour');
+const sizeRouter = require('./size');
 
 function setRoute (server) {
 
     server.use('/api/admin', adminRouter);
 
     server.use('/api/colour', colourRouter);
+
+    server.use('/api/size', sizeRouter);
 
 }
 
