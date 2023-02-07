da4ffd0cfcb4776d968da58275385f3c249455d2
Le Tan Phat <phatb1906740@student.ctu.edu.vn>
2023-02-07 20:21:15 +0700

    Tạo route colour

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const adminRouter = require('./admin');
+const colourRouter = require('./colour');
 
 function setRoute (server) {
 
     server.use('/api/admin', adminRouter);
+
+    server.use('/api/colour', colourRouter);
 
 }
 
