3916f905b6c116cc870574cbf28872fa9934b138
Le Tan Phat <phatb1906740@student.ctu.edu.vn>
2023-02-23 08:53:11 +0700

    Tao API list san pham cho trang customer

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -52,7 +52,64 @@ let listAdminSide = async (req, res, next) => {
 }
 
 let listCustomerSide = async (req, res, next) => {
-    return res.send('listCustomer');
+    try {
+        // Lấy danh sách tất cả sản phẩm ưu tiên sản phẩm mới nhất
+        let listProduct = await Product.findAll({ 
+            attributes: ['product_id'],
+            order: [ ['created_at', 'DESC'] ],
+            raw: true
+        });
+
+        let listProductVariant = [];
+
+        // Duyệt qua danh sách sản phẩm
+        for( let { product_id } of listProduct ) {
+            // Lấy danh sách tất cả các màu của sản phẩm đó
+            let listColor = await Product_Variant.findAll({
+                attributes: ['colour_id'],
+                where: { product_id },
+                group: [ 'colour_id' ],
+                raw: true
+            });
+            // Duyệt qua danh sách màu
+            for( let { colour_id } of listColor ) {
+                // Tìm tất cả biến thể sản phẩm có cùng màu với nhau
+                let listProductVariantSameColour = await Product_Variant.findAll({
+                    attributes: ['product_variant_id', 'colour_id'],
+                    include: [
+                        { model: Product, attributes: ['product_id', 'product_name', 'rating', 'sold', 'feedback_quantity'] },
+                        { model: Colour, attributes: ['colour_name'] },
+                        { model: Size, attributes: ['size_name'] },
+                        { model: Product_Variant_History, attributes: ['price'], separate: true, order: [ ['createdAt', 'DESC'] ] },
+                        { model: Product_Image, attributes: ['path'] },
+                    ],
+                    where: { colour_id },
+                });
+                // Convert dữ liệu
+                let productVariant = {
+                    product_id: listProductVariantSameColour[0].Product.product_id,
+                    product_name: listProductVariantSameColour[0].Product.product_name,
+                    rating: listProductVariantSameColour[0].Product.rating,
+                    sold: listProductVariantSameColour[0].Product.sold,
+                    feedback_quantity: listProductVariantSameColour[0].Product.feedback_quantity,
+                    product_variant_id: listProductVariantSameColour[0].product_variant_id,
+                    colour_id: listProductVariantSameColour[0].colour_id,
+                    colour_name: listProductVariantSameColour[0].Colour.colour_name,
+                    price: listProductVariantSameColour[0].Product_Variant_Histories[0].price,
+                    product_image: listProductVariantSameColour[0].Product_Images[0].path,
+                    sizes: []
+                };
+                // Duyệt qua danh sách biến thể sản phẩm có cùng màu để cộng dồn danh sách sizes
+                for( let { Size } of listProductVariantSameColour )
+                    productVariant.sizes.push(Size.size_name);
+                listProductVariant.push(productVariant);
+            }
+        }
+        return res.send(listProductVariant);
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send('Gặp lỗi khi tải dữ liệu vui lòng thử lại');
+    }
 }
 
 module.exports = {
