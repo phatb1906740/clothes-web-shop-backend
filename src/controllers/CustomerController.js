5e17fdace05db7a2ed64256a960b11e2dd0d94e5
Le Tan Phat <phatb1906740@student.ctu.edu.vn>
2023-03-09 11:38:27 +0700

    Sua api dang ky

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -5,19 +5,29 @@ const Customer_Info = require('../models/customer_info');
 
 let register = async (req, res, next) => {
     let email = req.body.email;
-    if(email === undefined) return res.status(400).send('Trường email không tồn tại');
+    if (email === undefined) return res.status(400).send('Trường email không tồn tại');
     let password = req.body.password;
-    if(password === undefined) return res.status(400).send('Trường password không tồn tại');
+    if (password === undefined) return res.status(400).send('Trường password không tồn tại');
+    let customer_name = req.body.customer_name;
+    if (customer_name === undefined) return res.status(400).send('Trường customer_name không tồn tại');
+    let phone_number = req.body.phone_number;
+    if (phone_number === undefined) return res.status(400).send('Trường phone_number không tồn tại');
+
     let customer = await User.findOne({ where: { email, role_id: 2 } });
-    if(customer) return res.status(409).send("Email đã tồn tại");
+    if (customer) return res.status(409).send("Email đã tồn tại");
     else {
         try {
             let hashPassword = bcrypt.hashSync(password, 10);
-            let newCustomer = { email: email, password: hashPassword, role_id: 2 };
-            let createCustomer = await User.create(newCustomer);
-            let newCustomerInfo = await Customer_Info.create({ user_id: createCustomer.user_id });
-            return res.send(createCustomer);
-        } catch(err) {
+            let newCustomer = await User.create({ email: email, password: hashPassword, role_id: 2 });
+            let newCustomerInfo = await Customer_Info.create({ user_id: newCustomer.user_id, customer_name, phone_number });
+            return res.send({
+                customer_id: newCustomer.user_id,
+                email: newCustomer.email,
+                customer_name: newCustomerInfo.customer_name,
+                phone_number: newCustomerInfo.phone_number,
+                address: newCustomerInfo.address
+            });
+        } catch (err) {
             console.log(err);
             return res.status(400).send("Có lỗi trong quá trình tạo tài khoản vui lòng thử lại");
         }
@@ -26,9 +36,9 @@ let register = async (req, res, next) => {
 
 let login = async (req, res, next) => {
     let email = req.body.email;
-    if(email === undefined) return res.status(400).send('Trường email không tồn tại');
+    if (email === undefined) return res.status(400).send('Trường email không tồn tại');
     let password = req.body.password;
-    if(password === undefined) return res.status(400).send('Trường password không tồn tại');
+    if (password === undefined) return res.status(400).send('Trường password không tồn tại');
 
     try {
         let customer = await User.findOne({
@@ -37,12 +47,12 @@ let login = async (req, res, next) => {
                 { model: Customer_Info, attributes: ['customer_name', 'phone_number', 'address'] },
             ]
         });
-        if(!customer) {
+        if (!customer) {
             return res.status(401).send("Email không chính xác");
         }
 
         let isPasswordValid = bcrypt.compareSync(password, customer.password);
-        if(!isPasswordValid) {
+        if (!isPasswordValid) {
             return res.status(401).send("Mật khẩu không chính xác");
         }
 
@@ -53,7 +63,7 @@ let login = async (req, res, next) => {
             phone_number: customer.Customer_Info.phone_number,
             address: customer.Customer_Info.address
         });
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.status(400).send("Có lỗi trong quá trình đăng nhập vui lòng thử lại");
     }
@@ -61,24 +71,24 @@ let login = async (req, res, next) => {
 
 let update = async (req, res, next) => {
     let user_id = req.body.user_id;
-    if(user_id === undefined) return res.status(400).send('Trường user_id không tồn tại');
+    if (user_id === undefined) return res.status(400).send('Trường user_id không tồn tại');
     let customer_name = req.body.customer_name;
-    if(customer_name === undefined) return res.status(400).send('Trường customer_name không tồn tại');
+    if (customer_name === undefined) return res.status(400).send('Trường customer_name không tồn tại');
     let phone_number = req.body.phone_number;
-    if(phone_number === undefined) return res.status(400).send('Trường phone_number không tồn tại');
+    if (phone_number === undefined) return res.status(400).send('Trường phone_number không tồn tại');
     let address = req.body.address;
-    if(address === undefined) return res.status(400).send('Trường address không tồn tại');
+    if (address === undefined) return res.status(400).send('Trường address không tồn tại');
 
     try {
         let customer = await User.findOne({ where: { user_id, role_id: 2 } });
-        if(!customer) return res.status(409).send("Customer không tồn tại");
+        if (!customer) return res.status(409).send("Customer không tồn tại");
 
         let numberUpdate = await Customer_Info.update(
             { customer_name, phone_number, address },
             { where: { user_id } }
         )
         return res.send(numberUpdate);
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.status(400).send("Có lỗi trong quá trình cập nhật vui lòng thử lại");
     }
--- src/models/customer_info.js
+++ src/models/customer_info.js
@@ -4,19 +4,19 @@ const { sequelize } = require('../configs/database');
 const User = require('./user');
 
 const Customer_Info = sequelize.define('Customer_Info', {
-    customer_info_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    customer_name: { type: DataTypes.STRING },
-    phone_number: { type: DataTypes.STRING },
-    address: { type: DataTypes.STRING },
+  customer_info_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  customer_name: { type: DataTypes.STRING, defaultValue: '' },
+  phone_number: { type: DataTypes.STRING, defaultValue: '' },
+  address: { type: DataTypes.STRING, defaultValue: '' },
 }, {
-    timestamps : false,
+  timestamps: false,
 })
 
 User.hasOne(Customer_Info, {
-    foreignKey: {  name: 'user_id', type: DataTypes.UUID, allowNull: false }
-  });
+  foreignKey: { name: 'user_id', type: DataTypes.UUID, allowNull: false }
+});
 Customer_Info.belongsTo(User, {
-    foreignKey: {  name: 'user_id', type: DataTypes.UUID, allowNull: false }
-  });
+  foreignKey: { name: 'user_id', type: DataTypes.UUID, allowNull: false }
+});
 
 module.exports = Customer_Info;
