9dabd5666b8d66c5fd41e1783a71ca05686e5596
Le Tan Phat <phatb1906740@student.ctu.edu.vn>
2023-03-16 10:32:34 +0700

    Tao api create order - 1

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,34 +1,94 @@
 const orderid = require('order-id')('key');
 
 const Order = require('../models/order');
+const User = require('../models/user');
 const Order_State = require('../models/order_state');
+const Product_Variant = require('../models/product_variant');
+const Product = require('../models/product');
+const Product_Price_History = require('../models/product_price_history');
+const Order_Item = require('../models/order_item');
 const Order_Status_Change_History = require('../models/order_status_change_history');
 
 let create = async (req, res, next) => {
+    let user_id = req.body.user_id;
+    if (user_id === undefined) return res.status(400).send('Trường user_id không tồn tại');
+    try {
+        let user = await User.findOne({ where: { user_id, role_id: 2 } });
+        if (user == null) return res.status(400).send('User này không tồn tại');
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send('Gặp lỗi khi tạo đơn hàng vui lòng thử lại');
+    }
     let customer_name = req.body.customer_name;
-    if (customer_name === undefined) return res.status(400).send('Trường tên customer_name không tồn tại');
+    if (customer_name === undefined) return res.status(400).send('Trường customer_name không tồn tại');
     let email = req.body.email;
-    if (email === undefined) return res.status(400).send('Trường tên email không tồn tại');
+    if (email === undefined) return res.status(400).send('Trường email không tồn tại');
     let phone_number = req.body.phone_number;
-    if (phone_number === undefined) return res.status(400).send('Trường tên phone_number không tồn tại');
+    if (phone_number === undefined) return res.status(400).send('Trường phone_number không tồn tại');
     let address = req.body.address;
-    if (address === undefined) return res.status(400).send('Trường tên address không tồn tại');
+    if (address === undefined) return res.status(400).send('Trường address không tồn tại');
+    let order_items = req.body.order_items;
+    if (order_items === undefined) return res.status(400).send('Trường order_items không tồn tại');
 
-    const order_id = orderid.generate().replace(/-/g, "");
-    let newOrder = await Order.create({
-        order_id,
-        customer_name,
-        email,
-        phone_number,
-        address,
-        total_product_value: 0,
-        delivery_charges: 20000,
-        total_oredr_value: 20000,
-    });
+    try {
+        let order_id = orderid.generate().replace(/-/g, "");
+        var newOrder = await Order.create({
+            user_id,
+            order_id,
+            customer_name,
+            email,
+            phone_number,
+            address,
+            total_product_value: 0,
+            delivery_charges: 0,
+            total_order_value: 0,
+        });
 
-    let state = await Order_State.findOne({ where: { state_id: 1, state_name: "Chờ Xác Nhận" } });
-    await newOrder.addOrder_State(state);
-    return res.send(newOrder)
+        let total_product_value = 0;
+        for (let i = 0; i < order_items.length; i++) {
+            let order_item = order_items[i];
+            let product_variant = await Product_Variant.findOne({
+                attributes: ['product_variant_id', 'quantity', 'state'],
+                include: [
+                    {
+                        model: Product, attributes: ['product_id'],
+                        include: { model: Product_Price_History, attributes: ['price'], separate: true, order: [['createdAt', 'DESC']] }
+                    },
+                ],
+                where: { product_variant_id: order_item.product_variant_id }
+            });
+            if (product_variant == null)
+                return res.status(400).send("Sản phẩm này không tồn tại");
+            if (product_variant.state != true)
+                return res.status(400).send("Sản phẩm này chưa được mở bán");
+            if (order_item.quantity > product_variant.quantity)
+                return res.status(400).send("Số lượng sản phẩm không hợp lệ");
+            let productVariantPrice = product_variant.Product.Product_Price_Histories[0].price;
+            let total_value = productVariantPrice * order_item.quantity;
+            let newOrderItem = {
+                order_id: newOrder.order_id,
+                product_variant_id: product_variant.product_variant_id,
+                order_item_index: i,
+                price: productVariantPrice,
+                quantity: order_item.quantity,
+                total_value
+            }
+            await Order_Item.create(newOrderItem);
+            newProductVariantQuantity = product_variant.quantity - order_item.quantity;
+            product_variant.update({ quantity: newProductVariantQuantity });
+            total_product_value += total_value;
+        }
+
+        let delivery_charges = 20000
+        let total_order_value = total_product_value + delivery_charges;
+        newOrder.update({ total_product_value, delivery_charges, total_order_value });
+        let state = await Order_State.findOne({ where: { state_id: 1, state_name: "Chờ Xác Nhận" } });
+        await newOrder.addOrder_State(state);
+        return res.send(newOrder)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send('Gặp lỗi khi tạo đơn hàng vui lòng thử lại');
+    }
 }
 
 module.exports = {
--- src/models/product.js
+++ src/models/product.js
@@ -4,25 +4,25 @@ const { sequelize } = require('../configs/database');
 const Category = require('./category');
 
 const Product = sequelize.define('Product', {
-    product_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    product_name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    rating: { type: DataTypes.FLOAT, defaultValue: 0 },
-    sold: { type: DataTypes.INTEGER, defaultValue: 0 },
-    feedback_quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+	product_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+	product_name: DataTypes.STRING,
+	description: DataTypes.TEXT,
+	rating: { type: DataTypes.FLOAT, defaultValue: 0 },
+	sold: { type: DataTypes.INTEGER, defaultValue: 0 },
+	feedback_quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
 }, {
-    timestamps : true,
-    createdAt: 'created_at',
-    updatedAt: false,
-    paranoid: true,
-    deletedAt: 'deleted_at'
+	timestamps: true,
+	createdAt: 'created_at',
+	updatedAt: false,
+	paranoid: true,
+	deletedAt: 'deleted_at'
 })
 
 Category.hasMany(Product, {
-    foreignKey: {  name: 'category_id', type: DataTypes.INTEGER}
-  });
+	foreignKey: { name: 'category_id', type: DataTypes.INTEGER }
+});
 Product.belongsTo(Category, {
-    foreignKey: {  name: 'category_id', type: DataTypes.INTEGER}
-  });
+	foreignKey: { name: 'category_id', type: DataTypes.INTEGER }
+});
 
 module.exports = Product;
